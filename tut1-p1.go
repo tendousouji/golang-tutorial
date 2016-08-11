e2f14cd08fa357eda1aadf0dfcc2178c9e9659b3
tendou <tendoumt1994@gmail.com>
2016-08-11 11:51:28 +0700

    finish exercise 5, 6 of tutorial 1

--- tut1-p1.go
+++ tut1-p1.go
@@ -11,7 +11,7 @@ func main() {
 	fmt.Printf("What is your name? ")
 	fmt.Scanf("%s", &myname)
 	fmt.Printf("Hello %s\n", myname)
-	fmt.Println("======================Excercise 2==========================================")
+	fmt.Println("======================Exercise 2==========================================")
 	//Ex2
 	var a, b, c, d, e float64
 	fmt.Printf("Input number a & b: ")
@@ -23,9 +23,9 @@ func main() {
 	fmt.Printf("Result of square a: %.1f\n", c)
 	fmt.Printf("Result of square b: %.1f\n", d)
 	fmt.Printf("Result of the math %.1f\n", e)
-	fmt.Println("======================Excercise 3==========================================")
+	fmt.Println("======================Exercise 3==========================================")
 	//Ex3
-	var f, g uint8
+	var f, g uint16
 	fmt.Printf("Input number c: ")
 	fmt.Scanf("%d", &f)
 	g = f * 2
@@ -35,6 +35,51 @@ func main() {
 	} else {
 		fmt.Println("The result is smaller than 100")
 	}
+	fmt.Println("======================Exercise 4==========================================")
+	//Ex4
+	var h uint16
+	fmt.Printf("Type the number of the month: ")
+	fmt.Scanf("%d", &h)
+	if h <= 12 && h >= 1 {
+		fmt.Printf("Number of the month: %d\n", h)
 
-	fmt.Println("================================================================")
+		if h >= 1 && h <= 3 {
+			fmt.Println("Season 1")
+		} else if h >= 4 && h <= 6 {
+			fmt.Println("Season 2")
+		} else if h >= 7 && h <= 9 {
+			fmt.Println("Season 3")
+		} else if h >= 10 && h <= 12 {
+			fmt.Println("Season 4")
+		}
+
+	} else {
+		fmt.Printf("The input number is not number of the month\n")
+	}
+	fmt.Println("======================Exercise 5==========================================")
+	//Ex5
+	var distance, price uint64
+	fmt.Printf("Type the distance: ")
+	fmt.Scanf("%d", &distance)
+	fmt.Printf("The distance is: %dkm\n", distance)
+
+	if distance <= 3 {
+		price = distance * 20500
+	} else if distance > 3 && distance <= 15 {
+		price = distance * 15350
+	} else if distance > 15 && distance <= 39 {
+		price = distance * 12999
+	} else if distance > 39 {
+		price = distance * 8000
+	}
+
+	if distance%10 == 0 {
+		price = price - price*20/100
+	}
+
+	fmt.Printf("The price is: %d VND\n", price)
+
+	fmt.Println("======================Exercise 6==========================================")
+	//Ex6
+	fmt.Println("==========================================================================")
 }
